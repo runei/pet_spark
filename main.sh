7826f07cbcc02043663de9c8607e299a69eac322
Leonardo Invernizzi <lminvernizzi@gmail.com>
2022-09-09 07:22:52 +0200

    initial commit

--- main.sh
+++ main.sh
@@ -3,9 +3,9 @@ if [ "$#" -ne 1 ]; then
 fi
 
 echo "${1-}" > "config.txt"
-exit
+
 dotnet build "StockAnalyzer/StockAnalyzer.csproj"
 
 spark-submit --class org.apache.spark.deploy.dotnet.DotnetRunner --master local StockAnalyzer/bin/Debug/net6.0/microsoft-spark-3-0_2.12-1.0.0.jar dotnet StockAnalyzer/bin/Debug/net6.0/StockAnalyzer.dll
 
-# python3 -m http.server
+python3 -m http.server
